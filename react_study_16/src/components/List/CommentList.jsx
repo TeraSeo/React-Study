962f752c4fb2946a3dce557d77bd93543ebd9932
TeraSeo <seotj0413@gmail.com>
2024-09-16 16:10:30 +0900

    React - Mini Project

---
+++ react_study_16/src/components/List/CommentList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import CommentListItem from "../Item/CommentListItem";
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+`;
+
+function CommentList(props) {
+    const { comments } = props;
+
+    return (
+        <Wrapper>
+            {
+                comments.map((comment, index) => {
+                    return (
+                        <CommentListItem comment={comment}></CommentListItem>
+                    );
+                })
+            }
+        </Wrapper>
+    );
+}
+
+export default CommentList;
